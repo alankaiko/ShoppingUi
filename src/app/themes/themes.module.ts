fd6766a1b835f156e7f4902d52520bbc7cc8a864
alankaiko <alamdfs@hotmail.com>
2024-05-14 14:55:51 -0300

    building core and environment

--- src/app/themes/themes.module.ts
+++ src/app/themes/themes.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LayoutComponent} from './layout/layout.component';
+import { MenuTopComponent } from './menu-top/menu-top.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    MenuTopComponent,
+    FooterComponent
   ],
   exports: [
     LayoutComponent
