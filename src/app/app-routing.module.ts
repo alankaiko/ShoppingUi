b6bf11a76883f41376bc052e509ca4f6123bd63b
alankaiko <alamdfs@hotmail.com>
2024-05-14 11:48:56 -0300

    initial configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LayoutComponent} from "./themes/layout/layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'institucional',
+        loadChildren: () => import('./institucional/institucional.module').then(m => m.InstitucionalModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'pages',
+        loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+      }
+    ]
+  },
+
+  {path: '', redirectTo: 'institucional/home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'pagina-nao-encontrada'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
